0cf6173ba4b36dccc9bd77a9c4dd83471c6d68a3
arklow.sei <raiceadship@gmail.com>
2024-03-15 09:06:45 +0000

    init commiit

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,18 @@
+// export const color = {
+//     primary: "#F0E4CF",
+//     secondary: "#0E1921",
+//     secondaryLight: "#1A2B37",
+//     black: "#0A0A0B",
+//     white: "#F8F7FF",
+//     whiteShade: "#A8ABB4",
+//     bg:"#0A141B",
+// }
+
 export const color = {
-    primary: "#F0E4CF",
-    secondary: "#0E1921",
-    secondaryLight: "#1A2B37",
-    black: "#0A0A0B",
+    primary: "#FFFF99", // text + icons + mint button
+    secondary: "#BB4411", // public + +/- buttons
+    secondaryLight: "#3366CC", //whitelist
+    black: "#000000",
     white: "#F8F7FF",
     whiteShade: "#A8ABB4",
     bg:"#0A141B",
